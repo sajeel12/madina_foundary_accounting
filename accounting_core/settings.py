4a4fa9d16f46572cdab9a751cae5b25752024896
sajeel12 <msajeelahmad2001@gmail.com>
2025-12-08 19:17:34 +0500

    s

--- accounting_core/settings.py
+++ accounting_core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'tailwind',
+    # 'tailwind',
     'theme',
     'django_browser_reload',
     'finance',
